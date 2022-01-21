090a2bbbfd5d426017f08ea7b7908bd69b5ffa1e
And-Salei <saleiandrew@gmail.com>
2022-01-21 02:06:21 +0200

    20-01-22

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+number = +prompt('Введите число');
+
+
+
+for (let item of number {
+    
+if (number % item == 0);
+    {console.log (`Число ${number} - простое`);}
+else if {console.log (`Число ${number} - непростое`);}
+
+}
+
